ebb4eed0cc10f2618dd9a9a1e44b5e15df45a995
Anwesh43 <anweshthecool0@gmail.com>
2018-03-30 07:23:12 +0530

    adding powerbuttonview module

--- powerbuttonmoveview/src/main/java/ui/anwesome/com/powerbuttonmoveview/PowerButtonMoveView.kt
+++ powerbuttonmoveview/src/main/java/ui/anwesome/com/powerbuttonmoveview/PowerButtonMoveView.kt
@@ -1,5 +1,22 @@
 package ui.anwesome.com.powerbuttonmoveview
 
+import android.content.Context
+import android.graphics.*
+import android.view.*
 /**
  * Created by anweshmishra on 30/03/18.
  */
+class PowerButtonMoveView (ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
