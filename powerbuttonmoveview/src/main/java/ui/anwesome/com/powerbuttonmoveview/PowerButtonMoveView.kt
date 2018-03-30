befb6ce6ff2f69a07bcc2e46f3483d19b2c41cdb
Anwesh43 <anweshthecool0@gmail.com>
2018-03-30 07:56:51 +0530

    adding PowerButtonMove class

--- powerbuttonmoveview/src/main/java/ui/anwesome/com/powerbuttonmoveview/PowerButtonMoveView.kt
+++ powerbuttonmoveview/src/main/java/ui/anwesome/com/powerbuttonmoveview/PowerButtonMoveView.kt
@@ -3,15 +3,17 @@ package ui.anwesome.com.powerbuttonmoveview
 import android.content.Context
 import android.graphics.*
 import android.view.*
+
 /**
  * Created by anweshmishra on 30/03/18.
  */
-class PowerButtonMoveView (ctx : Context) : View(ctx) {
-    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    override fun onDraw(canvas : Canvas) {
+class PowerButtonMoveView(ctx: Context) : View(ctx) {
+    val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas: Canvas) {
 
     }
-    override fun onTouchEvent(event : MotionEvent) : Boolean {
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
 
@@ -19,9 +21,10 @@ class PowerButtonMoveView (ctx : Context) : View(ctx) {
         }
         return true
     }
-    data class State(var  prevScale : Float = 0f, var dir : Int = 0, var j : Int = 0) {
-        val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f)
-        fun update(stopcb : (Float) -> Unit) {
+
+    data class State(var prevScale: Float = 0f, var dir: Int = 0, var j: Int = 0) {
+        val scales: Array<Float> = arrayOf(0f, 0f, 0f, 0f)
+        fun update(stopcb: (Float) -> Unit) {
             scales[j] += 0.1f * dir
             if (Math.abs(scales[j] - prevScale) > 1) {
                 prevScale = scales[j] + dir
@@ -34,36 +37,65 @@ class PowerButtonMoveView (ctx : Context) : View(ctx) {
                 }
             }
         }
-        fun startUpdating(startcb : () -> Unit) {
+
+        fun startUpdating(startcb: () -> Unit) {
             if (dir == 0) {
                 dir = 1 - 2 * prevScale.toInt()
                 startcb()
             }
         }
     }
-    data class Animator(var view : View, var animated : Boolean = false) {
-        fun animate(updatecb : () -> Unit) {
+
+    data class Animator(var view: View, var animated: Boolean = false) {
+        fun animate(updatecb: () -> Unit) {
             if (animated) {
                 try {
                     updatecb()
                     Thread.sleep(50)
                     view.invalidate()
-                }
-                catch (ex : Exception) {
+                } catch (ex: Exception) {
 
                 }
             }
         }
+
         fun start() {
             if (!animated) {
                 animated = true
                 view.postInvalidate()
             }
         }
+
         fun stop() {
             if (animated) {
                 animated = false
             }
+        }
+    }
+
+    data class PowerButtonMove(var i: Int, val state: State = State()) {
+        fun draw(canvas: Canvas, paint: Paint) {
+            paint.color = Color.WHITE
+            val w: Float = canvas.width.toFloat()
+            val h: Float = canvas.height.toFloat()
+            val r = Math.min(w, h) / 18
+            paint.strokeWidth = r / 3
+            val deg: Float = 15f
+            canvas.save()
+            canvas.translate(w / 2, h / 2)
+            canvas.rotate(90f * state.scales[2])
+            val y_updated = (h / 2) * state.scales[3]
+            canvas.drawLine(-y_updated, 0f, -y_updated + (r + r / 6) * state.scales[1], 0f, paint)
+            canvas.drawArc(RectF(-r, -r, r, r), deg, (360f - 2 * deg) * state.scales[0], false, paint)
+            canvas.restore()
+        }
+
+        fun update(stopcb: (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb: () -> Unit) {
+            state.startUpdating(startcb)
         }
     }
 }
