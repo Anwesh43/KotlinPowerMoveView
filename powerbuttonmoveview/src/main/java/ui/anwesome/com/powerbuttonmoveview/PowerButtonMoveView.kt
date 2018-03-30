558e4d38ba80eb430f5eca9a439ca3c76eb91c32
Anwesh43 <anweshthecool0@gmail.com>
2018-03-30 07:40:21 +0530

    adding State class

--- powerbuttonmoveview/src/main/java/ui/anwesome/com/powerbuttonmoveview/PowerButtonMoveView.kt
+++ powerbuttonmoveview/src/main/java/ui/anwesome/com/powerbuttonmoveview/PowerButtonMoveView.kt
@@ -14,9 +14,31 @@ class PowerButtonMoveView (ctx : Context) : View(ctx) {
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class State(var  prevScale : Float = 0f, var dir : Int = 0, var j : Int = 0) {
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                prevScale = scales[j] + dir
+                j += dir
+                if (j == scales.size || j == -1) {
+                    j -= dir
+                    dir = 0
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0) {
+                dir = 1 - 2 * prevScale.toInt()
+                startcb()
+            }
+        }
     }
 }
